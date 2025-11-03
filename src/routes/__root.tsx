6f2eb864f7f10f5383a029abbeb64639eb23d13b
Ray <hi@rmncldyo.com>
2025-11-03 11:02:59 -0500

    feat(routes): add query client context to root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Scripts, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { QueryClient } from '@tanstack/react-query'
 
 import Footer from '../components/Footer'
 import Header from '../components/Header'
@@ -8,7 +9,9 @@ import { ThemeProvider } from '../components/theme-provider'
 
 import appCss from '../styles.css?url'
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
   head: () => ({
     meta: [
       {
